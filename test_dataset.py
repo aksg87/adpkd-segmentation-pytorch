ce436bdd4a5d80e7f91501caa205fbac11cac1d6
Akshay Goel <akshay.k.goel@gmail.com>
2020-05-17 10:17:02 -0400

    feat: Test script for Dataset creation with train / val / test split

---
+++ test_dataset.py
@@ -0,0 +1,24 @@
+# %% 
+import torch
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+from data_set.datasets import SegmentationDataset
+from data.data_utils import make_dcmdicts, get_labeled
+from data.link_data import makelinks
+
+# %%
+makelinks()
+# %%
+# find number of patients
+_, patient2dcm = make_dcmdicts(tuple(get_labeled()))
+all_IDS = range(len(patient2dcm))
+
+# %%
+# train, val, test split--> 85 / 15 / 15. Note: Applied to patients not dcm images.
+train_IDS, test_IDS = train_test_split(all_IDS, test_size=0.15, random_state=1)
+train_IDS, val_IDS = train_test_split(train_IDS, test_size=0.176, random_state=1)
+
+# %%
+dataset_train = SegmentationDataset(patient_IDS=train_IDS)
+dataset_val = SegmentationDataset(patient_IDS=val_IDS)
