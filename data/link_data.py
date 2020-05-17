8ec4c5c63b1918d9cfc7771a73149c7e6ddb77b4
Akshay Goel <akshay.k.goel@gmail.com>
2020-05-17 01:27:58 -0400

    feat: Add mask .png symlinks to /data

--- data/link_data.py
+++ data/link_data.py
@@ -4,7 +4,7 @@ import glob
 import ntpath  
 import shutil
 
-from data_utils import get_dcms_paths
+from data_utils import get_dcms_paths, get_y_Path
 from data_config import labeled_dirs, unlabeled_dirs # define data sources in data_config.py
 #%%
 def symlink_force(target, link_name):
@@ -27,9 +27,21 @@ mkdir_force('labeled')
 mkdir_force('unlabeled')
  # %%
 for dcm in labeled_dcms:
+    mask = get_y_Path(dcm)
+
+    if not mask.exists():
+        raise Exception('Labeled dcm [{}] does not have mask.'.format(dcm))
+
     symlink_force(dcm, 'labeled/'+ os.path.basename(dcm))
+    symlink_force(mask, 'labeled/'+ os.path.basename(mask))
  # %%
 for dcm in unlabeled_dcms:
+    mask = get_y_Path(dcm)
+
+    if mask.exists():
+        raise Exception('Unlabeled dcm [{}] contains a mask.'.format(dcm))
+
     symlink_force(dcm, 'unlabeled/'+ os.path.basename(dcm))
+    symlink_force(mask, 'unlabeled/'+ os.path.basename(mask))
 
 # %%
