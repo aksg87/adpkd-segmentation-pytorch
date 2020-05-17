efd5fe408c78ad2cf6a101eb808416d4bdac1ff4
Akshay Goel <akshay.k.goel@gmail.com>
2020-05-17 01:24:07 -0400

    feat: Add mapping function from .dcm to .png mask

--- data/data_utils.py
+++ data/data_utils.py
@@ -1,7 +1,7 @@
 #%%
 import glob
 import os
-
+from pathlib import Path
 #%%
 
 def get_dcms_paths(dir_list):
@@ -17,3 +17,15 @@ def get_dcms_paths(dir_list):
         print("total files... --> {} \n".format(len(all_files)))
 
     return all_files
+
+def get_y_Path(x):
+    """Get label path from dicom path"""    
+
+    if isinstance(x, str):
+        x = Path(x)
+
+    y = str(x.absolute()).replace('DICOM_anon', 'Ground')
+    y = y.replace('.dcm', '.png')    
+    y = Path(y)
+
+    return y
