d0cbf999ff6201ede9e735bbafffbd9d96dd5189
Akshay Goel <akshay.k.goel@gmail.com>
2020-05-18 23:09:49 -0400

    feat: Test batch from dataloader with display

--- run_trainer.py
+++ run_trainer.py
@@ -33,3 +33,23 @@ train_IDS, val_IDS = train_test_split(train_IDS, test_size=0.176, random_state=1
 dataset_train = SegmentationDataset(patient_IDS=train_IDS, transform_x=T_x, transform_y=T_y)
 dataset_val = SegmentationDataset(patient_IDS=val_IDS, transform_x=T_x, transform_y=T_y)
 
+# %%
+
+# *** Needs Fix *** - dataset transforms break sample display
+# display_sample(dataset_train[160])
+# display_verbose_sample(dataset_train.get_verbose(160))
+
+# %%
+dataloaders = {
+    "train": DataLoader(dataset=dataset_train, batch_size=16, shuffle=True),
+    "val": DataLoader(dataset=dataset_val, batch_size=16, shuffle=True)
+}
+# %%
+data_iter = iter(dataloaders["train"])
+
+# %%
+for inputs, labels in data_iter:
+
+    display_traindata(inputs, labels)
+    break
+# %%
