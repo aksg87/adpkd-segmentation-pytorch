a21aae437261d793bee7de5aa07921adf10fe735
Akshay Goel <akshay.k.goel@gmail.com>
2020-05-19 02:49:52 -0400

    feat: Added model (SPM) and model.utils

---
+++ model/models.py
@@ -0,0 +1,14 @@
+# %%
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import numpy as np
+
+import segmentation_models_pytorch as smp
+
+from .model_utils import params
+
+# %%
+
+model = smp.Unet(**params)     
