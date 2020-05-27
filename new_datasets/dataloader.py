e8c32960fe4ae042b77a340c45f724b9e7254afc
Akshay Goel <akshay.k.goel@gmail.com>
2020-05-27 13:58:04 -0400

    add dataloader class for config

---
+++ new_datasets/dataloader.py
@@ -0,0 +1,27 @@
+from torch.utils.data import DataLoader
+
+
+class BaselineDataloaderGetter:
+    """create baseline dataloaders"""
+
+    def __init__(self, datasets, batchsize, shuffle):
+        """
+        Arguments:
+            datasets {list of dataset}
+            batchsize {int}
+            shuffle {boolean}
+        """
+        self.datasets = datasets
+        self.batchsize = batchsize
+        self.shuffle = shuffle
+
+    def __call__(self):
+
+        return [
+            DataLoader(
+                dataset=dataset,
+                batch_size=self.batchsize,
+                shuffle=self.shuffle,
+            )
+            for dataset in self.datasets
+        ]  # convert to dictionary with names in datasets?
