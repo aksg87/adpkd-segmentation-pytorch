216e0b5c13a74c12df6f2ed30caf58ae0b7f23ef
Akshay Goel <akshay.k.goel@gmail.com>
2020-05-27 02:40:48 -0400

    update splits with size calc and data init

--- new_datasets/splits.py
+++ new_datasets/splits.py
@@ -1,15 +1,23 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
 
+from data.link_data import makelinks
+from data.data_utils import make_dcmdicts, get_labeled
+
 class GenSplit():
 
-    def __init__(self, size, train=0.7, val=0.15, test=0.15, seed=1):
+    
+    def __init__(self, train=0.7, val=0.15, test=0.15, seed=1):
         super().__init__()
+
+        makelinks()
+        # size needs to be be calculated for splits
+        self.dcm2attribs, self.patient2dcm = make_dcmdicts(tuple(get_labeled()))
 
         self.train = train
         self.val = val 
         self.test = test
-        all_idxs = range(size)
+        all_idxs = range(len(self.patient2dcm.keys()))
 
         # split train from validation-test
         train_idxs, test_val_idxs = train_test_split(all_idxs, test_size=(self.val + self.test), random_state=seed)
