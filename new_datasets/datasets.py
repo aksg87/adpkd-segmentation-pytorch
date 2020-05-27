9f4c1b405761f8c11be0bf4929b4716848336362
Akshay Goel <akshay.k.goel@gmail.com>
2020-05-27 15:22:49 +0200

    dataset config updates

--- new_datasets/datasets.py
+++ new_datasets/datasets.py
@@ -6,31 +6,21 @@ import torchvision.transforms.functional as TF
 import data_set.datasets as ds
 
 # %%
-class BaselineDatasetGetter(torch.utils.data.Dataset):
-    """Create baseline segmentation dataset"""
+class BaselineDatasetGetter:
+    """Create baseline segmentation datasets"""
 
-    def __init__(self, params):
+    def __init__(self, splitter, transform_x, transform_y, preprocess_func):
         super().__init__()
-        self.idxs = params["idxs"]
-        self.hyperparams = params["hyperparams"]
-        self.transform_x = params["transform_x"]
-        self.transform_y = params["transform_y"]
-        self.preprocess_func = params["proprocessing_func"]
+        self.splitter = splitter()
+        self.transform_x = transform_x()
+        self.transform_y = transform_y()
+        self.preprocess_func = preprocess_func()
 
     def __call__(self):
-        
-        # allows for passing objects or lists/directionaries for respective variables
-        if callable(self.idxs):
-            self.idxs = self.idxs()
-        
-        if callable(self.hyperparam_gen):
-            self.hyperparam = self.hyperparams()
 
-        if callable(self.transform_x):
-            self.transform_x = self.transform_x()
-        
-        if callable(self.transform_y):
-            self.transform_y = self.transform_y()
-
-        return ds.SegmentationDataset(patient_IDS=self.idxs, hyperparams=self.hyperparams, transform_x = self.transform_x, transform_y =self.transform_x, processing = self.preprocess_func)
-        
+        return [ds.SegmentationDataset(
+            patient_IDS=indices,
+            transform_x=self.transform_x,
+            transform_y=self.transform_y,
+            processing=self.preprocess_func,
+        ) for indices in self.splitter]
--- new_datasets/splits.py
+++ new_datasets/splits.py
@@ -1,37 +1,41 @@
-import numpy as np
 from sklearn.model_selection import train_test_split
 
-from data.link_data import makelinks
 from data.data_utils import make_dcmdicts, get_labeled
 
-class GenSplit():
 
-    
+class GenSplit:
     def __init__(self, train=0.7, val=0.15, test=0.15, seed=1):
         super().__init__()
 
-        makelinks()
         # size needs to be be calculated for splits
-        self.dcm2attribs, self.patient2dcm = make_dcmdicts(tuple(get_labeled()))
+        self.dcm2attribs, self.patient2dcm = make_dcmdicts(
+            tuple(get_labeled())
+        )
 
         self.train = train
-        self.val = val 
+        self.val = val
         self.test = test
         all_idxs = range(len(self.patient2dcm.keys()))
 
         # split train from validation-test
-        train_idxs, test_val_idxs = train_test_split(all_idxs, test_size=(self.val + self.test), random_state=seed)
+        train_idxs, test_val_idxs = train_test_split(
+            all_idxs, test_size=(self.val + self.test), random_state=seed
+        )
 
         # split validation from test
-        val_idxs, test_idxs = train_test_split(test_val_idxs, test_size=(self.test / (self.test + self.val)), random_state=seed)     
+        val_idxs, test_idxs = train_test_split(
+            test_val_idxs,
+            test_size=(self.test / (self.test + self.val)),
+            random_state=seed,
+        )
 
         self.train_idxs = train_idxs
         self.val_idxs = val_idxs
         self.test_idxs = test_idxs
 
-    def __call__(self, type="train"):
-        
-        if type=="train":
+    def __call__(self, type_="train"):
+
+        if type_ == "train":
             return self.train_idxs
         else:
             return self.train_idxs, self.val_idxs, self.test_idxs
