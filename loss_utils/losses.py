455f163ca8a318c19732f4b5ab0967271edf6470
Akshay Goel <akshay.k.goel@gmail.com>
2020-05-30 12:05:56 -0400

    temp assert check in losses.py

--- loss_utils/losses.py
+++ loss_utils/losses.py
@@ -10,19 +10,21 @@ def dice_loss(pred, target, smooth=1e-8):
     pred = pred.view(-1)
     target = target.view(-1)
     intersection = (pred * target).sum()
-    dice = (2.*intersection + smooth)/(pred.sum() + target.sum() + smooth)
+    dice = (2.0 * intersection + smooth) / (pred.sum() + target.sum() + smooth)
     return 1 - dice
 
 
 # current criterion as example
 class BaselineLoss(nn.Module):
     "Baseline loss criterion callable"
+
     def __init__(self, dice_smooth=1e-8, bce_weight=0.5):
         super().__init__()
         self.dice_smooth = dice_smooth
         self.bce_weight = bce_weight
 
     def __call__(self, pred, target):
+        assert (target >= 0).all().item()
         bce = F.binary_cross_entropy_with_logits(pred, target)
 
         pred = F.sigmoid(pred)
