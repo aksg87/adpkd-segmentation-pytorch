94dc6a8c4c693bde38f7978df0c9d665da585d6b
Akshay Goel <akshay.k.goel@gmail.com>
2020-06-04 00:17:45 -0400

    add channel wise loss

--- loss_utils/losses.py
+++ loss_utils/losses.py
@@ -1,5 +1,6 @@
 """Loss utilities and definitions"""
 
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -12,6 +13,30 @@ def dice_loss(pred, target, smooth=1e-8):
     intersection = (pred * target).sum()
     dice = (2.0 * intersection + smooth) / (pred.sum() + target.sum() + smooth)
     return 1 - dice
+
+
+def soft_dice_loss(y_pred, y_true, epsilon=1e-6, X_Y=(2, 3)):
+    """
+    Soft dice loss calculation for arbitrary batch size, number of classes, and number of spatial dimensions.
+    Assumes the `channels_first` format by default: X_Y = (2, 3)
+
+    # Arguments
+
+        y_pred: b x c x (X x Y) Network output, must sum to 1 over
+        y_true: b x c x (X x Y) One hot encoding of ground truth
+        c channel (such as after softmax)
+        epsilon: Used for numerical stability to avoid divide by zero errors
+
+    # Reference in Numpy
+        https://gist.github.com/jeremyjordan/9ea3032a32909f71dd2ab35fe3bacc08
+    """
+
+    numerator = 2.0 * torch.sum(y_pred * y_true, X_Y)
+    denominator = torch.sum(torch.pow(y_pred, 2) + torch.pow(y_true, 2), X_Y)
+
+    return 1 - torch.mean(
+        numerator / (denominator + epsilon)
+    )  # average over classes and batch
 
 
 # current criterion as example
