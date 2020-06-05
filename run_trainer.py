731e136f44c19a2e305b23022d460355cc15e845
Akshay Goel <akshay.k.goel@gmail.com>
2020-06-05 11:18:44 -0400

    old run_trainer.py and trainer.py fixes

--- run_trainer.py
+++ run_trainer.py
@@ -1,20 +1,16 @@
-# %% 
+# %%
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
+
 import torch.optim as optim
 from torch.utils.data import DataLoader
-from torchvision import transforms
 
-import numpy as np
 from sklearn.model_selection import train_test_split
 
 from data_set.datasets import SegmentationDataset
-from data.data_utils import make_dcmdicts, get_labeled, display_traindata, filter_dcm2attribs
+from data.data_utils import make_dcmdicts, get_labeled, display_traindata
 from data.link_data import makelinks
 
-from data_set.datasets import SegmentationDataset
-from data_set.transforms import T_x, T_y 
+from data_set.transforms import T_x, T_y
 from trainer.trainer import training_loop, criterion
 from model.models import model
 from model.model_utils import get_preprocessing, preprocessing_fn
@@ -27,17 +23,31 @@ _, patient2dcm = make_dcmdicts(tuple(get_labeled()))
 all_IDS = range(len(patient2dcm))
 
 # %%
-# train, val, test split--> 85 / 15 / 15. Note: Applied to patients not dcm images.
+# train, val, test split--> 85 / 15 / 15.
+# Note: Applied to patients not dcm images.
 train_IDS, test_IDS = train_test_split(all_IDS, test_size=0.15, random_state=1)
-train_IDS, val_IDS = train_test_split(train_IDS, test_size=0.176, random_state=1)
+train_IDS, val_IDS = train_test_split(
+    train_IDS, test_size=0.176, random_state=1
+)
 
 # %%
-dataset_train = SegmentationDataset(patient_IDS=train_IDS, transform_x=T_x, transform_y=T_y, preprocessing=get_preprocessing(preprocessing_fn))
-dataset_val = SegmentationDataset(patient_IDS=val_IDS, transform_x=T_x, transform_y=T_y, preprocessing=get_preprocessing(preprocessing_fn))
+dataset_train = SegmentationDataset(
+    patient_IDS=train_IDS,
+    transform_x=T_x,
+    transform_y=T_y,
+    preprocessing=get_preprocessing(preprocessing_fn),
+)
+dataset_val = SegmentationDataset(
+    patient_IDS=val_IDS,
+    transform_x=T_x,
+    transform_y=T_y,
+    preprocessing=get_preprocessing(preprocessing_fn),
+)
 
 # %%
-print("image -> shape {},  dtype {}".format(x.shape, x.dtype))
-print("mask -> shape {},  dtype {}".format(y.shape, y.dtype))
+# Needs fix: x and y not defined
+# print("image -> shape {},  dtype {}".format(x.shape, x.dtype))
+# print("mask -> shape {},  dtype {}".format(y.shape, y.dtype))
 
 # %%
 # *** Needs Fix *** - dataset transforms break sample display
@@ -47,7 +57,7 @@ print("mask -> shape {},  dtype {}".format(y.shape, y.dtype))
 # %%
 dataloaders = {
     "train": DataLoader(dataset=dataset_train, batch_size=16, shuffle=True),
-    "val": DataLoader(dataset=dataset_val, batch_size=16, shuffle=True)
+    "val": DataLoader(dataset=dataset_val, batch_size=16, shuffle=True),
 }
 # %%
 data_iter = iter(dataloaders["train"])
@@ -57,9 +67,10 @@ for inputs, labels in data_iter:
     display_traindata(inputs, labels)
     break
 # %%
-optimizer_ft = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()))
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
+# optimizer should be set after the model is placed on the correct device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+optimizer_ft = optim.Adam(
+    filter(lambda p: p.requires_grad, model.parameters())
+)
 training_loop(model, criterion, optimizer_ft, 30, dataloaders, device)
-
-# %%
--- trainer/trainer.py
+++ trainer/trainer.py
@@ -1,24 +1,15 @@
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-
-import numpy as np
-
-from tqdm.auto import trange, tqdm
-
-import time
-from datetime import datetime
-from collections import defaultdict
+import torch.functional as F
 
 
-def dice_loss(pred, target, smooth = 1e-8):
-     #flatten label and prediction tensors
+def dice_loss(pred, target, smooth=1e-8):
+    # flatten label and prediction tensors
     pred = pred.view(-1)
     target = target.view(-1)
-    intersection = (pred * target).sum()                            
-    dice = (2.*intersection + smooth)/(pred.sum() + target.sum() + smooth)   
+    intersection = (pred * target).sum()
+    dice = (2.0 * intersection + smooth) / (pred.sum() + target.sum() + smooth)
     return 1 - dice
+
 
 def criterion(pred, target, bce_weight=0.5):
     bce = F.binary_cross_entropy_with_logits(pred, target)
@@ -29,64 +20,69 @@ def criterion(pred, target, bce_weight=0.5):
 
     # metrics['loss'] += loss.data.cpu().numpy() * target.size(0)
     # metrics['bce'] += bce.data.cpu().numpy() * target.size(0)
-    # metrics['dice'] += dice.data.cpu().numpy() * target.size(0)    
+    # metrics['dice'] += dice.data.cpu().numpy() * target.size(0)
 
     return loss
 
-def make_train_step(model, criterion, optimizer):
-    """Builds function that performs a step in the train loop"""
-    def train_step(x, y):
-
-        model.train()
-        yhat = model(x)
-
-        loss = criterion(y, yhat)
-        loss.backward()
-        optimizer.step()
-        optimizer.zero_grad()
-
-        return loss.item()
-    
-    # Returns a function --> called within training loop
-    return train_step
-
 
 def training_loop(model, criterion, optimizer, n_epochs, dataloaders, device):
-    
-    model = model.to(device)
 
-    train_losses = []
-    val_losses = []
-    train_step = make_train_step(model, criterion, optimizer)
+    def train_step(x, y):
+        optimizer.zero_grad()
+        yhat = model(x)
+        loss = criterion(yhat, y)
+        loss.backward()
+        optimizer.step()
+        return loss.item()
 
     train_loader = dataloaders["train"]
     val_loader = dataloaders["val"]
 
     for epoch in range(n_epochs):
-        
-        train_samples, val_samples = 0, 0
+        model.train()
+        # epoch batch averages
+        val_losses = []
+        train_losses = []
+        train_samples = 0
+        val_samples = 0
 
         for x_batch, y_batch in train_loader:
             x_batch = x_batch.to(device)
             y_batch = y_batch.to(device)
+            train_step(x_batch, y_batch)
 
-            train_loss = train_step(x_batch, y_batch)
-            train_losses.append(train_loss * y_batch.size(0)) # NO NEED FOR .data.cpu().numpy()? ****
-            train_samples += y_batch.size(0)
-            
         with torch.no_grad():
-            for x_val, y_val in val_loader:
-                x_val = x_val.to(device)
-                y_val = y_val.to(device)
-                
+            for x, y in val_loader:
+                x = x.to(device)
+                y = y.to(device)
+
                 model.eval()
+                yhat = model(x)
+                loss = criterion(yhat, y)
+                val_losses.append(loss.item() * y.size(0))
+                val_samples += y.size(0)
 
-                yhat = model(x_val)
-                val_loss = criterion(y_val, yhat)
-                val_losses.append(val_loss * y_batch.size(0)) # NO NEED FOR .data.cpu().numpy()? ****
-                val_samples += y_batch.size(0)
+        with torch.no_grad():
+            # check performance on the entire train set at the end of the epoch
+            for x, y in train_loader:
+                x = x.to(device)
+                y = y.to(device)
 
-        print("Epoch {}:  Train Losses: {}\n".format(epoch, np.sum(train_losses) / train_samples))
-        print("Epoch {}:  Val Losses:   {}\n".format(epoch, np.sum(val_losses) / val_samples))
+                model.eval()
+                yhat = model(x)
+                loss = criterion(yhat, y)
+                train_losses.append(loss.item() * y.size(0))
+                train_samples += y.size(0)
+
+        print(
+            "Epoch {}:  Train Losses: {}\n".format(
+                epoch, sum(train_losses) / train_samples
+            )
+        )
+        print(
+            "Epoch {}:  Val Losses:   {}\n".format(
+                epoch, sum(val_losses) / val_samples
+            )
+        )
 
     print(model.state_dict())
