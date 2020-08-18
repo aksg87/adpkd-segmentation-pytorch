3b93fc01c0eddd89b248b9bcacdc980c541cdf3c
Akshay Goel <akshay.k.goel@gmail.com>
2020-08-18 10:25:53 -0400

    use pathlib for data path specifications

--- adpkd_segmentation/config/data_config_example.py
+++ adpkd_segmentation/config/data_config_example.py
@@ -1,13 +1,15 @@
-import os
+from pathlib import Path
 
 # %%
-# this path should be adapted to the location of your data
-# after modification, rename and place this file to `./data/data_config.py`
-dataroot = "/root/data/"
+# modify according to the location of your data
+
+# after modification, rename and place this file as
+# adpkd_segmentation.data.data_config.py
+dataroot = Path("/root/data/")
 
 labeled_dirs = [
-    os.path.join(dataroot, "training-data-01-60MR"),
-    os.path.join(dataroot, "training_data-61-110MR_AX_SSFSE_ABD_PEL_50"),
+    dataroot / "training-data-01-60MR",
+    dataroot / "training_data-61-110MR_AX_SSFSE_ABD_PEL_50",
 ]
 
-unlabeled_dirs = [os.path.join(dataroot, "unlabelled_data")]
+unlabeled_dirs = [dataroot / "unlabelled_data"]
--- adpkd_segmentation/data/data_config.py
+++ adpkd_segmentation/data/data_config.py
@@ -1,11 +1,11 @@
-import os
+from pathlib import Path
 
 # %%
-dataroot = "data_copy/"
+dataroot = Path("data_copy/")
 
 labeled_dirs = [
-    os.path.join(dataroot, "training-data-01-60MR"),
-    os.path.join(dataroot, "training_data-61-110MR_AX_SSFSE_ABD_PEL_50"),
+    dataroot / "training-data-01-60MR",
+    dataroot / "training_data-61-110MR_AX_SSFSE_ABD_PEL_50",
 ]
 
-unlabeled_dirs = [os.path.join(dataroot, "unlabelled_data")]
+unlabeled_dirs = [dataroot / "unlabelled_data"]
