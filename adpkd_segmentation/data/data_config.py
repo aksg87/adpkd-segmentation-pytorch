af6c23f3c1adfd83bfdb4a32884a1d28a136b48d
Akshay Goel <akshay.k.goel@gmail.com>
2020-09-02 07:19:30 -0400

    data config update

--- adpkd_segmentation/data/data_config.py
+++ adpkd_segmentation/data/data_config.py
@@ -2,12 +2,14 @@ from pathlib import Path
 
 # %%
 # adapt the file for actual data locations
-dataroot = Path("data_copy/")
+# dataroot = Path("data_copy/")
+dataroot = Path("data")
 
-labeled_dirs = [
-    dataroot / "training-data-01-60MR",
-    dataroot / "training_data-61-110MR_AX_SSFSE_ABD_PEL_50",
-]
+# labeled_dirs = [
+#     dataroot / "training-data-01-60MR",
+#     dataroot / "training_data-61-110MR_AX_SSFSE_ABD_PEL_50",
+# ]
+labeled_dirs = [dataroot / "processed_studies"]
 unlabeled_dirs = [dataroot / "unlabelled_data"]
 
 # modify for custom symbolic link locations
