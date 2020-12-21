11f3078dbce9be04c62bf5a6b89ff9aa7be452a6
Akshay Goel <akshay.k.goel@gmail.com>
2020-12-21 00:32:27 -0500

    Multi model inference
    TODO: Refactor notebook

--- notebooks/evaluate_patients_notebook.py
+++ notebooks/evaluate_patients_notebook.py
@@ -8,6 +8,7 @@ The makelinks flag is needed only once to create symbolic links to the data.
 from collections import OrderedDict, defaultdict
 
 import pandas as pd
+import cv2
 import torch
 import os
 import yaml
@@ -33,7 +34,6 @@ from adpkd_segmentation.utils.stats_utils import (  # noqa
 )
 
 from adpkd_segmentation.utils.losses import SigmoidBinarize, Dice  # noqa
-
 from torch.nn import Sigmoid
 
 # %%
@@ -211,7 +211,7 @@ def inference_to_disk(
                 )
 
                 out_dir.parent.mkdir(parents=True, exist_ok=True)
-                print(out_dir)
+                # print(out_dir)
 
                 np.save(str(out_dir) + "_img", img.cpu().numpy())
                 np.save(str(out_dir) + "_pred", pred.cpu().numpy())
@@ -243,13 +243,53 @@ def inference_to_disk(
 # %%
 
 
-def compute_inference_stats(save_dir="./saved_inference", output=None):
+def resized_stack(numpy_list, dsize=None):
+    """resizing lists of array with dimension:
+    slices x 1 x H' x W'.
+
+    H' x W' default to the first array or dsize
+    """
+    assert numpy_list[0].shape[1] == 1, "dimension check"
+    assert numpy_list[0].shape[2] == numpy_list[0].shape[3], "dimension check"
+
+    def reshape(arr):
+        """reshapes to dimension:
+        H x W x slices
+        """
+        arr = np.moveaxis(arr, 0, -1)
+        arr = np.squeeze(arr)
+        return arr
+    
+    reshaped = [reshape(arr) for arr in numpy_list]
+
+    if dsize is None:
+        dsize = reshaped[0].shape[0: 2]
+
+    resized = [cv2.resize(src, dsize, interpolation = cv2.INTER_CUBIC) for src in reshaped]
+
+    return np.stack(resized)
+
+def compute_inference_stats(save_dir="./saved_inference", output=False, display=False):
     Metric_data = OrderedDict()
+    Combined_metric_data = OrderedDict()
     root = Path.cwd() / Path(save_dir)
 
-    models = root.glob("*")
+    model_inferences = list(root.glob("*"))
 
-    for model_dir in models:
+    print(f"calculating model inferences for... {model_inferences}\n\n")
+
+    all_pred_vol = defaultdict(list)
+    all_ground_vol = defaultdict(list)
+    all_summaries = defaultdict(list)
+
+    pred_process = SigmoidBinarize(thresholds=[0.5])
+    dice = Dice(
+        pred_process=pred_process, use_as_loss=False, power=1, dim=(0, 1, 2, 3)
+    )
+
+    res = []
+
+    for model_dir in model_inferences:
         patient_ID, MR_num = "*", "*"
         studies = model_dir.glob(f"{patient_ID}/{MR_num}")
 
@@ -265,49 +305,50 @@ def compute_inference_stats(save_dir="./saved_inference", output=None):
 
             attribs = sorted(study_dir.glob("*_attrib.json"))
 
+            # volumes for a study within one model inference
             img_vol = np.stack(imgs_np)
             pred_vol = np.stack(preds_np)
             ground_vol = np.stack(grounds_np)
 
-            print(img_vol.shape, pred_vol.shape, ground_vol.shape)
+            if not res:
+                res.append(pred_vol)
+            # accumulate volumes across all models for each study
+            rel_path = study_dir.relative_to(model_dir)
+            all_pred_vol[rel_path].append(pred_vol)
+            all_ground_vol[rel_path].append(ground_vol)
 
-            # Compute Stats Next
-            pred_process = SigmoidBinarize(thresholds=[0.5])
-            dice = Dice(
-                pred_process=pred_process, use_as_loss=False, power=1, dim=(0, 1, 2, 3)
-            )
+            if display is True:
+                slice1 = 1 * (pred_vol.shape[0] // 6)
+                slice2 = 2 * (pred_vol.shape[0] // 6)
+                slice3 = 3 * (pred_vol.shape[0] // 6)
+                slice4 = 4 * (pred_vol.shape[0] // 6)
+                slice5 = 5 * (pred_vol.shape[0] // 6)
 
-            slice1 = 1 * (pred_vol.shape[0] // 6)
-            slice2 = 2 * (pred_vol.shape[0] // 6)
-            slice3 = 3 * (pred_vol.shape[0] // 6)
-            slice4 = 4 * (pred_vol.shape[0] // 6)
-            slice5 = 5 * (pred_vol.shape[0] // 6)
+                f, ax = plt.subplots(5, 2)
+                
+                ax[0, 0].imshow(img_vol[slice1, 0], cmap="gray")
+                ax[0, 1].imshow(img_vol[slice1, 0], cmap="gray")
+                ax[1, 0].imshow(img_vol[slice2, 0], cmap="gray")
+                ax[1, 1].imshow(img_vol[slice2, 0], cmap="gray")
+                ax[2, 0].imshow(img_vol[slice3, 0], cmap="gray")
+                ax[2, 1].imshow(img_vol[slice3, 0], cmap="gray")
+                ax[3, 0].imshow(img_vol[slice4, 0], cmap="gray")
+                ax[3, 1].imshow(img_vol[slice4, 0], cmap="gray")
+                ax[4, 0].imshow(img_vol[slice5, 0], cmap="gray")
+                ax[4, 1].imshow(img_vol[slice5, 0], cmap="gray")
 
-            f, ax = plt.subplots(5, 2)
+                ax[0, 0].imshow(pred_vol[slice1, 0], cmap="viridis", alpha=0.3)
+                ax[0, 1].imshow(ground_vol[slice1, 0], cmap="viridis", alpha=0.3)
+                ax[1, 0].imshow(pred_vol[slice2, 0], cmap="viridis", alpha=0.3)
+                ax[1, 1].imshow(ground_vol[slice2, 0], cmap="viridis", alpha=0.3)
+                ax[2, 0].imshow(pred_vol[slice3, 0], cmap="viridis", alpha=0.3)
+                ax[2, 1].imshow(ground_vol[slice3, 0], cmap="viridis", alpha=0.3)
+                ax[3, 0].imshow(pred_vol[slice4, 0], cmap="viridis", alpha=0.3)
+                ax[3, 1].imshow(ground_vol[slice4, 0], cmap="viridis", alpha=0.3)
+                ax[4, 0].imshow(pred_vol[slice5, 0], cmap="viridis", alpha=0.3)
+                ax[4, 1].imshow(ground_vol[slice5, 0], cmap="viridis", alpha=0.3)
 
-            ax[0, 0].imshow(img_vol[slice1, 0], cmap="gray")
-            ax[0, 1].imshow(img_vol[slice1, 0], cmap="gray")
-            ax[1, 0].imshow(img_vol[slice2, 0], cmap="gray")
-            ax[1, 1].imshow(img_vol[slice2, 0], cmap="gray")
-            ax[2, 0].imshow(img_vol[slice3, 0], cmap="gray")
-            ax[2, 1].imshow(img_vol[slice3, 0], cmap="gray")
-            ax[3, 0].imshow(img_vol[slice4, 0], cmap="gray")
-            ax[3, 1].imshow(img_vol[slice4, 0], cmap="gray")
-            ax[4, 0].imshow(img_vol[slice5, 0], cmap="gray")
-            ax[4, 1].imshow(img_vol[slice5, 0], cmap="gray")
-
-            ax[0, 0].imshow(pred_vol[slice1, 0], cmap="viridis", alpha=0.3)
-            ax[0, 1].imshow(ground_vol[slice1, 0], cmap="viridis", alpha=0.3)
-            ax[1, 0].imshow(pred_vol[slice2, 0], cmap="viridis", alpha=0.3)
-            ax[1, 1].imshow(ground_vol[slice2, 0], cmap="viridis", alpha=0.3)
-            ax[2, 0].imshow(pred_vol[slice3, 0], cmap="viridis", alpha=0.3)
-            ax[2, 1].imshow(ground_vol[slice3, 0], cmap="viridis", alpha=0.3)
-            ax[3, 0].imshow(pred_vol[slice4, 0], cmap="viridis", alpha=0.3)
-            ax[3, 1].imshow(ground_vol[slice4, 0], cmap="viridis", alpha=0.3)
-            ax[4, 0].imshow(pred_vol[slice5, 0], cmap="viridis", alpha=0.3)
-            ax[4, 1].imshow(ground_vol[slice5, 0], cmap="viridis", alpha=0.3)
-
-            f.tight_layout()
+                f.tight_layout()
 
             dice_val = dice(torch.from_numpy(pred_vol), torch.from_numpy(ground_vol)).item()
 
@@ -319,7 +360,7 @@ def compute_inference_stats(save_dir="./saved_inference", output=None):
                 scale_factor = (attrib["dim"][0] ** 2) / (
                     attrib["transform_resize_dim"][0] ** 2
                 )
-
+                # print(f"scale factor {scale_factor}")
                 pred_pixel_count = torch.sum(pred_process(torch.from_numpy(pred_vol))).item()
                 volume_pred = scale_factor * attrib["vox_vol"] * pred_pixel_count
 
@@ -328,7 +369,7 @@ def compute_inference_stats(save_dir="./saved_inference", output=None):
                 ).item()
                 volume_ground = scale_factor * attrib["vox_vol"] * ground_pixel_count
 
-                print(f"volume_pred:{volume_pred}  volume_ground:{volume_ground}")
+                # print(f"volume_pred:{volume_pred}  volume_ground:{volume_ground}")
 
                 summary = {
                     "TKV_GT": volume_ground,
@@ -336,27 +377,65 @@ def compute_inference_stats(save_dir="./saved_inference", output=None):
                     "sequence": attrib["seq"],
                     "split": split,
                     "patient_dice": dice_val,
+                    "study": attrib["patient"] + attrib["MR"],
+                    "scale_factor": scale_factor,
+                    "vox_vol": attrib["vox_vol"],
+                    "dim": attrib["dim"][0],
+                    "transform_resize_dim": attrib["transform_resize_dim"][0]
                 }
 
                 study = attrib["patient"] + attrib["MR"]
                 Metric_data[study] = summary
-        
+
+                all_summaries[rel_path].append(summary)
+
+
         df = pd.DataFrame(Metric_data).transpose()
 
-        if output is not None:
-            df.to_csv(output)
+        if output is True:
+            df.to_csv(f"stats-{model_dir.name}.csv")
 
-            # ground_vol =
 
-            # scale_factor = (attribs["dim"][0] ** 2) / (
-            #     attribs["transform_resize_dim"][0] ** 2
-            # )
-            # attribs["Vol_GT"] = (
-            #     scale_factor
-            #     * attribs["vox_vol"]
-            #     * attribs["ground_kidney_pixels"]
-            # )
+    for key, value in all_pred_vol.items():
+        all_pred_vol[key] = resized_stack(value)
+        summary = all_summaries[key][0]
+        pred_vol = np.mean(all_pred_vol[key], axis=0)
+        pred_std = np.std(all_pred_vol[key])
 
+        # move back to slices x 1 x H x W
+        pred_vol = np.moveaxis(pred_vol, -1, 0)
+        pred_vol = np.expand_dims(pred_vol, axis=1)
+        
+        # res.append(pred_vol)
+        # return res
+        ground_vol = all_ground_vol[key][0]
+
+        # print(f"scale factor {scale_factor}")
+        pred_pixel_count = torch.sum(pred_process(torch.from_numpy(pred_vol))).item()
+        scale_factor = summary["scale_factor"] 
+        vox_vol = summary["vox_vol"]
+
+        volume_pred = scale_factor * vox_vol * pred_pixel_count
+        ground_pixel_count = torch.sum(
+            pred_process(torch.from_numpy(ground_vol))
+        ).item()
+        volume_ground = scale_factor * vox_vol * ground_pixel_count
+
+        dice_val = dice(torch.from_numpy(pred_vol), torch.from_numpy(ground_vol)).item()
+
+        summary["TKV_Pred"] = volume_pred
+        summary["TKV_GT"] = volume_ground 
+        summary["patient_dice"] = dice_val
+        summary["Pred_stdev"] = pred_std  
+
+        Combined_metric_data[summary["study"]] = summary
+
+    df = pd.DataFrame(Combined_metric_data).transpose()
+
+    if output is True:
+        df.to_csv(f"stats-combined_models.csv")
+
+    return pred_std
 
 # %%
 
@@ -391,8 +470,11 @@ def load_config(config_path, run_makelinks=False):
     model = model.to(device)
     model.eval()
 
-    return dataloader, model, device, binarize_func, split
+    model_name = Path(config_path).parts[-3]
 
+    save_dir = "./saved_inference"
+
+    return dataloader, model, device, binarize_func, save_dir, model_name, split
 
 # %%
 def calculate_patient_metrics(updated_dcm2attrib, output=None):
@@ -482,23 +564,41 @@ def calculate_patient_metrics(updated_dcm2attrib, output=None):
 
 # path = "./example_experiment/train_example_all_no_noise_patient_seq_norm_b5_BN/val/val.yaml"  # noqa
 
-path = "./experiments/november/26_new_stratified_run_2_long/test/test.yaml"
+# path = "./experiments/november/26_new_stratified_run_2_long/test/test.yaml"
 
-# path = "./experiments/november/26_new_stratified_run_2_long_512/test/test.yaml"
+path = "./experiments/november/26_new_stratified_run_2_long_512/test/test.yaml"
 
-dataloader, model, device, binarize_func, split = load_config(config_path=path)
+paths = [
+    "./experiments/november/25_new_stratified_run_1/test/test.yaml",
+    "./experiments/november/25_new_stratified_run_2/test/test.yaml",
+    "./experiments/november/25_new_stratified_run_2_long/test/test.yaml",
+    "./experiments/november/26_new_stratified_run_2/test/test.yaml",
+    "./experiments/november/26_new_stratified_run_2_long/test/test.yaml",
+    "./experiments/november/26_new_stratified_run_2_long_512/test/test.yaml",
+    "./experiments/november/26_new_stratified_run_2_long_512_b6/test/test.yaml",
+    "./experiments/november/26_new_stratified_run_2_long_advprop/test/test.yaml",
+    "./experiments/november/26_new_stratified_run_2_long_batchdice1/test/test.yaml",
+    "./experiments/november/26_new_stratified_run_2_long_noisy-student/test/test.yaml"    
+]
+
+
+*model_args, split = load_config(config_path=path)
+
+# %%
+all_loaded_configs = [load_config(config_path=p) for p in paths]
+
+# img, pred, ground = visualize_inference(dataloader, model, device, binarize_func)
+
+# %%
+for loaded_configs in all_loaded_configs:
+    *model_args, split = loaded_configs
+    inference_to_disk(*model_args)
+
 
 # %%
 
-img, pred, ground = visualize_inference(dataloader, model, device, binarize_func)
-
-# %%
-
-inference_to_disk(dataloader, model, device, binarize_func)
-
-# %%
-
-compute_inference_stats(output="test-data_second.csv")
+test = compute_inference_stats(
+    output= True)
 
 
 # %%
