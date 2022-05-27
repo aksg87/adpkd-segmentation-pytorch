0d8f134d062c2cddbe5a346e03c5d1acc45d01ec
Dominick Romano <djr4003@med.cornell.edu>
2022-05-26 22:36:33 -0400

    Implemented basic addition ensemble. Overlap and organ repaint in next commit.

--- adpkd_segmentation/inference/add_ensemble.py
+++ adpkd_segmentation/inference/add_ensemble.py
@@ -2,14 +2,17 @@ import json
 from argparse import ArgumentParser
 import os
 from pathlib import Path
+import nibabel as nib
 
 from inference import run_inference
+from ensemble_utils import get_scan, addition_ensemble
 
 
 # Preliminary Data
 print("Loading system and pipeline configuration...")
-id_system = open("adpkd_segmentation/inference/ensemble_config.json", "r")
-system_config = json.loads(id_system.read())
+config_path = "adpkd_segmentation/inference/ensemble_config.json"
+with open(config_path, "r") as id_system:
+    system_config = json.loads(id_system.read())
 # Parser Setup
 parser = ArgumentParser()
 parser.add_argument(
@@ -45,13 +48,46 @@ def run_addition_ensemble(input_path=None, output_path=None):
             inference_path=input_path,
             saved_inference=saved_inference,
             saved_figs=saved_figs,
-        )  # Test this tomorrow
-        pred_load_dir.append(save_path)
-        print(system_config["organ_name"][idx_organ] + " inference complete")
+        )
+        pred_load_dir.append(Path(save_path))
+        print(name_organ + " inference complete")
         #
-        # Addition Ensemble -- Add in v1.02
+    # Create ensemble save path
+    temp_name = ""
+    if len(system_config["organ_name"]) <= system_config["max_organ_title"]:
+        for name in system_config["organ_name"]:
+            temp_name += f"_{name}"
+    else:
+        temp_name = "_organs"
+    combined_folder_name = f"Addition_Ensemble{temp_name}"
+    combine_path = Path(output_path) / combined_folder_name
+    # Addition Ensemble
+    print("Combining the organ segmentations...")
+    scan_list = list(pred_load_dir[0].glob(f'**/*{system_config["pred_vol"]}'))
+    for idScn, scan in enumerate(scan_list):
+        temp_dict = {"organ_paths": pred_load_dir, "idS": idScn}
+        scan_folder = get_scan(system_config, scan)
+        print(f"Combining for scan {scan_folder}")
+        comb_mask = addition_ensemble(temp_dict, system_config)
 
-    # Save the output -- Add in v1.02 or v1.03
+        # Save the output
+        save_path = combine_path / scan_folder
+        save_path.mkdir(parents=True, exist_ok=True)
+        #
+        dicom_parent, _ = os.path.split(scan)
+        dicom_nii_dir = Path(dicom_parent) / system_config["dicom_vol"]
+        mri_nifti = nib.load(dicom_nii_dir)
+        nifti_affine = mri_nifti.affine
+        nifti_header = mri_nifti.header.copy()
+        combined_pred_vol = nib.Nifti1Image(
+            comb_mask, affine=nifti_affine, header=nifti_header
+        )
+        print("Saving...")
+        nib.save(mri_nifti, save_path / system_config["dicom_vol"])
+        nib.save(
+            combined_pred_vol,
+            save_path / system_config["combined_pred_filename"],
+        )
 
 
 if __name__ == "__main__":
--- adpkd_segmentation/inference/ensemble_utils.py
+++ adpkd_segmentation/inference/ensemble_utils.py
@@ -1,2 +1,90 @@
-import glob
 import os
+from pathlib import Path
+import nibabel as nib
+import numpy as np
+
+
+def get_tail(dir=".", depth=1):
+    """
+    Given a folder path OR directory, return
+    the folder given the depth.
+    Note: depth = 1 denotes the newest folder
+    in the tree.
+    """
+    t_head = dir
+    num_depth = depth
+    if os.path.isfile(dir):
+        num_depth += 1
+    #
+    idx = 0
+    while idx < num_depth:  # test
+        t_head, t_tail = os.path.split(t_head)
+        idx += 1
+    #
+    return t_tail  # Tested, good
+
+
+def get_scan(config, dir="."):
+    """This is designed for the output folder pattern in
+    ADPKD-segmentation-pytorch (Goel et al. Radiology AI). At inference, the
+    nifti files are saved in
+    .../saved_inference/repo_name/patient_ID/scan_folder/ITKSNAP_DCM_NIFTI/pred.vol.nii
+    You can find Dr. Goel's repo at:
+    https://github.com/aksg87/adpkd-segmentation-pytorch.git
+    And you can find his deployment:
+    web: https://pubs.rsna.org/doi/10.1148/ryai.210205
+    doi: https://doi.org/10.1148/ryai.210205
+    """
+    target_depth = 1
+    if get_tail(dir, target_depth) == config["youngest_child"]:
+        target_depth += 1
+    return get_tail(dir, target_depth)
+
+
+def grab_organ_dirs(iter_dict, config):
+    """Given the initial parent path,
+    we will provide the directories to
+    all files + scans"""
+    path_dict = {}
+    if config["mode"] == "ensemble addition":
+        for organ, organ_path in zip(
+            config["organ_name"], iter_dict["organ_paths"]
+        ):
+            path_dict[organ] = list(
+                Path(organ_path).glob(f"**/*{config['pred_vol']}")
+            )
+        return path_dict  # TEST THIS
+    # To Do: if config['mode'] = "ensemble softmax"
+
+
+def mask_add(iter_dict, config):
+    idScan = iter_dict["idS"]  # Scan index
+    for idO, organ, add_color in zip(
+        range(len(config["organ_name"])),
+        config["organ_name"],
+        config["add_organ_color"],
+    ):
+        load_dir = iter_dict["mask_directories"][organ][idScan]
+        tmp_nii = nib.load(load_dir)
+        tmp_arr = np.array(tmp_nii.dataobj)
+        if idO == 0:
+            temp_combine = np.zeros(np.shape(tmp_arr))
+        temp_combine += add_color * tmp_arr
+
+    return np.uint16(temp_combine)
+
+
+def addition_ensemble(iter_dict, config):
+    """Given a dictionary that temporarily holds
+    the organ paths and scan index,"""
+    # Current commit: finalize mask_add
+    # Next commit:
+    #   -overlap_repaint
+    #   -organ_repaint
+    iter_dict["mask_directories"] = grab_organ_dirs(iter_dict, config)
+    overlap_mask = mask_add(iter_dict, config)
+    return overlap_mask  # For testing purposes -- v1_002 only
+    # Coming soon -- v1_003
+    # process_org_color_mask = overlap_repaint(overlap_mask,config,other_args...)
+    # comb_mask = organ_repaint(process_org_color_mask,config)
+    # return comb_mask
